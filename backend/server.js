fc95adf32c7f6b4010bc624d71e2e72664b048f0
Pedro Parodi Ramirez <pparodi@teceng-gaming.com.ar>
2023-02-27 14:47:49 -0300

    Update URL server for request.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import app from "./app.js";
 import { initSocket } from "./socket.js";
 
-const server = app.listen();
+const PORT = process.env.PORT;
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http esta escuchando en el puerto ${server.address().port}`);
+    console.log(`http://localhost:${server.address().port}`);
+});
 server.on("error", error => console.log(`Error en servidor ${error}`));
 initSocket(server);
